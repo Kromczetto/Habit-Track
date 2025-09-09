8899eddac0078fe49c8289699fe776959e909017
Kromczetto <jakubkromolowski@gmail.com>
2025-09-09 23:00:47 +0200

    Initial Commit

---
+++ Habit Track/Habit_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackApp.swift
+//  Habit Track
+//
+//  Created by Kuba Kromołowski on 09/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
