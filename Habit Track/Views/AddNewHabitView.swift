b7e73f0c223c278f80cb07d4af577168358573da
Kromczetto <jakubkromolowski@gmail.com>
2025-09-17 18:51:48 +0200

    Add new view responsible for adding new habit with the priority

---
+++ Habit Track/Views/AddNewHabitView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewHabitView.swift
+//  Habit Track
+//
+//  Created by Kuba Kromołowski on 10/09/2025.
+//
+
+import SwiftUI
+
+struct AddNewHabitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewHabitView()
+}
